76e81c95568e76e6f219ed59aa903f914aa4ffad
gwonhong.dev@gmail.com <gwonhong.dev@gmail.com>
2024-11-14 14:08:18 +0900

    init

---
+++ src/main/java/com/ssafy/travlog/api/TravlogApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.travlog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravlogApplication.class, args);
+    }
+
+}
