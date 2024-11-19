1ec09027075300d72d347fa21ab6afe422f855e4
Gwon Hong <gwonhong.dev@gmail.com>
2024-11-19 23:35:42 +0900

    Update DotenvConfig.java

--- src/main/java/com/ssafy/travlog/api/config/DotenvConfig.java
+++ src/main/java/com/ssafy/travlog/api/config/DotenvConfig.java
@@ -3,12 +3,9 @@ package com.ssafy.travlog.api.config;
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev") // Only active in the dev profile
 public class DotenvConfig {
-
     @PostConstruct
     public void loadEnv() {
         Dotenv dotenv = Dotenv.load();
