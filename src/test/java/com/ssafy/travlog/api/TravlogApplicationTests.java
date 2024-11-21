ef47793cdad9d96561699d6932a2693a9f5f6096
gwonhong.dev@gmail.com <gwonhong.dev@gmail.com>
2024-11-21 10:19:51 +0900

    remove test temporarily

--- src/test/java/com/ssafy/travlog/api/TravlogApplicationTests.java
+++ src/test/java/com/ssafy/travlog/api/TravlogApplicationTests.java
@@ -1,13 +1,12 @@
 package com.ssafy.travlog.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TravlogApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
