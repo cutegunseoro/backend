bad4e59bd5f3290c6ca7aec8117348ff940f8ed5
gwonhong.dev@gmail.com <gwonhong.dev@gmail.com>
2024-11-21 16:21:34 +0900

    remove service interface since not necessary for now

--- src/main/java/com/ssafy/travlog/api/service/MemberService.java
+++ src/main/java/com/ssafy/travlog/api/service/MemberService.java
@@ -3,16 +3,47 @@ package com.ssafy.travlog.api.service;
 import com.ssafy.travlog.api.dto.auth.LoginRequest;
 import com.ssafy.travlog.api.dto.auth.SignupRequest;
 import com.ssafy.travlog.api.dto.member.MemberInfo;
+import com.ssafy.travlog.api.mapper.MemberMapper;
+import com.ssafy.travlog.api.model.MemberModel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-public interface MemberService {
+@Service
+@RequiredArgsConstructor
+public class MemberService {
 
-    MemberInfo getMemberByMemberId(long memberId);
+    private final PasswordEncoder passwordEncoder;
+    private final MemberMapper memberMapper;
 
-    MemberInfo getMemberByLoginId(String loginId);
+    public MemberInfo getMemberByMemberId(long memberId) {
+        MemberModel memberModel = memberMapper.selectMemberByMemberId(memberId);
+        return new MemberInfo(memberModel.getPublicId(), memberModel.getDisplayName(), memberModel.getBio());
+    }
 
-    MemberInfo getMemberByPublicId(String publicId);
+    public MemberInfo getMemberByLoginId(String loginId) {
+        MemberModel memberModel = memberMapper.selectMemberByLoginId(loginId);
+        return new MemberInfo(memberModel.getPublicId(), memberModel.getDisplayName(), memberModel.getBio());
+    }
 
-    MemberInfo login(LoginRequest loginRequest);
+    public MemberInfo getMemberByPublicId(String publicId) {
+        MemberModel memberModel = memberMapper.selectMemberByPublicId(publicId);
+        return new MemberInfo(memberModel.getPublicId(), memberModel.getDisplayName(), memberModel.getBio());
+    }
 
-    int signup(SignupRequest signupRequest);
+    public MemberInfo login(LoginRequest loginRequest) {
+        MemberModel memberModel = memberMapper.selectMemberByLoginId(loginRequest.getLoginId());
+        if (passwordEncoder.matches(loginRequest.getPassword(), memberModel.getHashedPassword())) {
+            return new MemberInfo(memberModel.getPublicId(), memberModel.getDisplayName(), memberModel.getBio());
+        }
+        return null;
+    }
+
+    public int signup(SignupRequest signupRequest) {
+        MemberModel memberModel = new MemberModel();
+        memberModel.setLoginId(signupRequest.getLoginId());
+        memberModel.setHashedPassword(passwordEncoder.encode(signupRequest.getPassword()));
+        memberModel.setPublicId(signupRequest.getPublicId());
+        return memberMapper.insertMember(memberModel);
+    }
 }
