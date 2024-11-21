c24ceb7d9591b223d3dfaea3fe251e7915061758
gwonhong.dev@gmail.com <gwonhong.dev@gmail.com>
2024-11-21 11:35:32 +0900

    separated MembersMeController from MembersController

--- src/main/java/com/ssafy/travlog/api/controller/MembersMeController.java
+++ src/main/java/com/ssafy/travlog/api/controller/MembersMeController.java
@@ -5,19 +5,22 @@ import com.ssafy.travlog.api.dto.MemberInfoResponse;
 import com.ssafy.travlog.api.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/members")
+@RequestMapping("/members/me")
 @RequiredArgsConstructor
-public class MemberController {
+public class MembersMeController {
     private final MemberService memberService;
 
-    @GetMapping("/{publicId}")
-    public ResponseEntity<MemberInfoResponse> getUserInfo(@PathVariable String publicId) {
+    @GetMapping
+    public ResponseEntity<MemberInfoResponse> getMyInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String publicId = authentication.getName();
         MemberInfo memberInfo = memberService.getMemberByPublicId(publicId);
         if (memberInfo == null) {
             return ResponseEntity.notFound().build();
