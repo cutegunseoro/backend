615edd5015744aacbd085768de98cf719c4e8267
gwonhong.dev@gmail.com <gwonhong.dev@gmail.com>
2024-11-21 15:03:55 +0900

    remove me alias

--- src/main/java/com/ssafy/travlog/api/controller/MemberController.java
+++ src/main/java/com/ssafy/travlog/api/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.ssafy.travlog.api.dto.MemberInfoResponse;
 import com.ssafy.travlog.api.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,8 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/{publicId}")
-    public ResponseEntity<MemberInfoResponse> getUserInfo(@PathVariable String publicId) {
+    public ResponseEntity<MemberInfoResponse> getMemberInfo(@PathVariable String publicId, Authentication authentication) {
+        String tokenPublicId = authentication.getName();
         MemberInfo memberInfo = memberService.getMemberByPublicId(publicId);
         if (memberInfo == null) {
             return ResponseEntity.notFound().build();
