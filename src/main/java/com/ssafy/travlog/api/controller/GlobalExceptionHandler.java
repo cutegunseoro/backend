b735e3d001498556198c55b5fd5c64be0db5bccb
gwonhong.dev@gmail.com <gwonhong.dev@gmail.com>
2024-11-21 17:46:55 +0900

    GlobalExceptionHandler placeholder

---
+++ src/main/java/com/ssafy/travlog/api/controller/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.ssafy.travlog.api.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+}
