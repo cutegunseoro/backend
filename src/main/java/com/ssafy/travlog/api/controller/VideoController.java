8715d9c01904dfd9678d191bd16ed585389238ef
gwonhong.dev@gmail.com <gwonhong.dev@gmail.com>
2024-11-22 09:25:30 +0900

    created controller for video metadata upload

--- src/main/java/com/ssafy/travlog/api/controller/VideoController.java
+++ src/main/java/com/ssafy/travlog/api/controller/VideoController.java
@@ -1,13 +1,11 @@
 package com.ssafy.travlog.api.controller;
 
+import com.ssafy.travlog.api.dto.video.VideoMetaInsertRequest;
 import com.ssafy.travlog.api.service.VideoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -23,5 +21,14 @@ public class VideoController {
     public ResponseEntity<String> uploadVideo(@RequestParam("file") MultipartFile file) throws IOException {
         String videoUrl = videoService.saveVideo(file);
         return ResponseEntity.ok(videoUrl);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> uploadVideoMetadata(@RequestBody VideoMetaInsertRequest videoMetaInsertRequest) {
+        int result = videoService.uploadVideoMetadata(videoMetaInsertRequest);
+        if (result == 0) {
+            return ResponseEntity.badRequest().body("Failed to upload video metadata");
+        }
+        return ResponseEntity.ok("Video metadata uploaded successfully");
     }
 }
