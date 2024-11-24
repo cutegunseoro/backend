4852f2a9c6b01b27db5fc8ab94033e59656303eb
Gwon Hong <gwonhong.dev@gmail.com>
2024-11-24 17:46:54 +0900

    implemented getVideoMetadataListOfTravel

--- src/main/java/com/ssafy/travlog/api/controller/TravelController.java
+++ src/main/java/com/ssafy/travlog/api/controller/TravelController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.travlog.api.dto.travel.TravelAddRequest;
 import com.ssafy.travlog.api.dto.travel.TravelInfoResponse;
+import com.ssafy.travlog.api.dto.video.VideoMetadataListResponse;
 import com.ssafy.travlog.api.service.TravelService;
+import com.ssafy.travlog.api.service.VideoService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TravelController {
 	private final TravelService travelService;
+	private final VideoService videoService;
 
 	@PostMapping
 	public ResponseEntity<Void> addTravel(Authentication authentication,
@@ -33,6 +36,12 @@ public class TravelController {
 	@GetMapping("/{travelId}")
 	public ResponseEntity<TravelInfoResponse> getTravelInfo(@PathVariable Long travelId) {
 		var res = travelService.getTravelInfo(travelId);
+		return ResponseEntity.ok().body(res);
+	}
+
+	@GetMapping("/{travelId}/videos")
+	public ResponseEntity<VideoMetadataListResponse> getVideoMetadataListOfTravel(@PathVariable Long travelId) {
+		var res = videoService.getVideoMetadataListOfTravel(travelId);
 		return ResponseEntity.ok().body(res);
 	}
 }
