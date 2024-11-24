0ce02ab7836e9d978baa2515a253cb470152343f
Gwon Hong <gwonhong.dev@gmail.com>
2024-11-24 19:57:35 +0900

    add null handling to FileUtil

--- src/main/java/com/ssafy/travlog/api/util/FileUtil.java
+++ src/main/java/com/ssafy/travlog/api/util/FileUtil.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class FileUtil {
-    public String getFileExtension(String fileName) {
-        int dotIndex = fileName.lastIndexOf('.');
-        return (dotIndex >= 0) ? fileName.substring(dotIndex) : ""; // Include the dot (e.g., ".mp4")
-    }
+	public String getFileExtension(String fileName) {
+		if (fileName == null || !fileName.contains(".")) {
+			return null;
+		}
+		int dotIndex = fileName.lastIndexOf('.');
+		return (dotIndex >= 0) ? fileName.substring(dotIndex) : ""; // Include the dot (e.g., ".mp4")
+	}
 }
