6720e23ded1a79c8088b4f42731c4c95b12601ad
Gwon Hong <gwonhong.dev@gmail.com>
2024-11-24 19:54:47 +0900

    done

--- src/main/java/com/ssafy/travlog/api/controller/VideoController.java
+++ src/main/java/com/ssafy/travlog/api/controller/VideoController.java
@@ -1,48 +1,70 @@
 package com.ssafy.travlog.api.controller;
 
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.ssafy.travlog.api.dto.video.VideoFileUploadResponse;
+import com.ssafy.travlog.api.dto.video.VideoMetadataListResponse;
 import com.ssafy.travlog.api.dto.video.VideoMetadataResponse;
 import com.ssafy.travlog.api.dto.video.VideoMetadataUploadRequest;
 import com.ssafy.travlog.api.service.VideoService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @Slf4j
 @RestController
 @RequestMapping("/videos")
 @RequiredArgsConstructor
 public class VideoController {
-    private final VideoService videoService;
+	private final VideoService videoService;
 
-    @PostMapping("/upload")
-    public ResponseEntity<VideoFileUploadResponse> uploadVideoFile(
-            Authentication authentication,
-            @RequestParam("file") MultipartFile file
-    ) throws IOException {
-        VideoFileUploadResponse res = videoService.uploadVideoFile(authentication, file);
-        return ResponseEntity.ok(res);
-    }
+	@PostMapping("/upload")
+	public ResponseEntity<VideoFileUploadResponse> uploadVideoFile(
+		Authentication authentication,
+		@RequestParam("file") MultipartFile file
+	) throws IOException {
+		VideoFileUploadResponse res = videoService.uploadVideoFile(authentication, file);
+		return ResponseEntity.ok(res);
+	}
 
-    @PutMapping
-    public ResponseEntity<Void> uploadVideoMetadata(
-            Authentication authentication,
-            @RequestBody VideoMetadataUploadRequest videoMetadataUploadRequest
-    ) {
-        videoService.uploadVideoMetadata(authentication, videoMetadataUploadRequest);
-        return ResponseEntity.ok().build();
-    }
+	@PutMapping
+	public ResponseEntity<Void> uploadVideoMetadata(
+		Authentication authentication,
+		@RequestBody VideoMetadataUploadRequest videoMetadataUploadRequest
+	) {
+		videoService.uploadVideoMetadata(authentication, videoMetadataUploadRequest);
+		return ResponseEntity.ok().build();
+	}
 
-    @GetMapping("/{videoId}")
-    public ResponseEntity<VideoMetadataResponse> getVideoMetadata(
-            @PathVariable Long videoId
-    ) {
-        var res = videoService.getVideoMetadata(videoId);
-        return ResponseEntity.ok(res);
-    }
+	@GetMapping("/{videoId}")
+	public ResponseEntity<VideoMetadataResponse> getVideoMetadata(
+		@PathVariable Long videoId
+	) {
+		var res = videoService.getVideoMetadata(videoId);
+		return ResponseEntity.ok(res);
+	}
+
+	@GetMapping("/search")
+	public ResponseEntity<VideoMetadataListResponse> searchVideoMetadata(
+		@RequestParam String coordinates,
+		@RequestParam(required = false) Integer distance
+	) {
+		if (coordinates != null && !coordinates.isEmpty()) {
+			var res = videoService.getVideoMetadataListByLocationAndDistance(coordinates, distance);
+			return ResponseEntity.ok(res);
+		}
+		return ResponseEntity.badRequest().build();
+	}
 }
--- src/main/java/com/ssafy/travlog/api/service/VideoService.java
+++ src/main/java/com/ssafy/travlog/api/service/VideoService.java
@@ -1,106 +1,129 @@
 package com.ssafy.travlog.api.service;
 
-import com.github.f4b6a3.uuid.UuidCreator;
-import com.ssafy.travlog.api.dto.video.*;
-import com.ssafy.travlog.api.mapper.VideoMapper;
-import com.ssafy.travlog.api.model.VideoInsertModel;
-import com.ssafy.travlog.api.model.VideoModel;
-import com.ssafy.travlog.api.util.FileUtil;
-import com.ssafy.travlog.api.util.MemberUtil;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.github.f4b6a3.uuid.UuidCreator;
+import com.ssafy.travlog.api.dto.video.VideoFileUploadResponse;
+import com.ssafy.travlog.api.dto.video.VideoMetadata;
+import com.ssafy.travlog.api.dto.video.VideoMetadataListResponse;
+import com.ssafy.travlog.api.dto.video.VideoMetadataResponse;
+import com.ssafy.travlog.api.dto.video.VideoMetadataUploadRequest;
+import com.ssafy.travlog.api.mapper.VideoMapper;
+import com.ssafy.travlog.api.model.VideoInsertModel;
+import com.ssafy.travlog.api.model.VideoModel;
+import com.ssafy.travlog.api.util.FileUtil;
+import com.ssafy.travlog.api.util.MemberUtil;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
 @Service
 @RequiredArgsConstructor
 public class VideoService {
-    private final VideoMapper videoMapper;
+	private final VideoMapper videoMapper;
 
-    private final FileUtil fileUtil;
-    private final MemberUtil memberUtil;
+	private final FileUtil fileUtil;
+	private final MemberUtil memberUtil;
 
-    private final String videoDir = System.getProperty("user.dir") + "/uploads/videos";
+	private final String videoDir = System.getProperty("user.dir") + "/uploads/videos";
 
-    @PostConstruct
-    public void init() {
-        File dir = new File(videoDir);
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
-    }
+	@PostConstruct
+	public void init() {
+		File dir = new File(videoDir);
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+	}
 
-    public VideoFileUploadResponse uploadVideoFile(
-            Authentication authenticaiton,
-            MultipartFile file
-    ) throws IOException {
-        if (file == null || file.isEmpty()) {
-            throw new IllegalArgumentException("File is null");
-        }
+	public VideoFileUploadResponse uploadVideoFile(
+		Authentication authenticaiton,
+		MultipartFile file
+	) throws IOException {
+		if (file == null || file.isEmpty()) {
+			throw new IllegalArgumentException("File is null");
+		}
 
-        // TODO: implement authentication check (rate limit, etc.)
+		// TODO: implement authentication check (rate limit, etc.)
 
-        String filePath = UuidCreator.getTimeOrderedEpoch().toString() + fileUtil.getFileExtension(file.getOriginalFilename());
+		String filePath =
+			UuidCreator.getTimeOrderedEpoch().toString() + fileUtil.getFileExtension(file.getOriginalFilename());
 
-        file.transferTo(Paths.get(videoDir, filePath).toFile());
-        return new VideoFileUploadResponse(filePath);
-    }
+		file.transferTo(Paths.get(videoDir, filePath).toFile());
+		return new VideoFileUploadResponse(filePath);
+	}
 
-    public void uploadVideoMetadata(
-            Authentication authentication,
-            VideoMetadataUploadRequest videoMetadataUploadRequest
-    ) {
+	public void uploadVideoMetadata(
+		Authentication authentication,
+		VideoMetadataUploadRequest videoMetadataUploadRequest
+	) {
 
-        VideoInsertModel videoInsertModel = VideoInsertModel.builder()
-                .memberId(memberUtil.getMemberIdFromAuthentication(authentication))
-                .travelId(videoMetadataUploadRequest.getTravelId())
-                .coordinates(videoMetadataUploadRequest.getCoordinates())
-                .videoUrl(videoMetadataUploadRequest.getVideoUrl())
-                .thumbnailUrl(videoMetadataUploadRequest.getThumbnailUrl())
-                .title(videoMetadataUploadRequest.getTitle())
-                .description(videoMetadataUploadRequest.getDescription())
-                .build();
-        int result = videoMapper.insertVideo(videoInsertModel);
-        if (result != 1) {
-            throw new RuntimeException("Failed to insert video metadata");
-        }
-    }
+		VideoInsertModel videoInsertModel = VideoInsertModel.builder()
+			.memberId(memberUtil.getMemberIdFromAuthentication(authentication))
+			.travelId(videoMetadataUploadRequest.getTravelId())
+			.coordinates(videoMetadataUploadRequest.getCoordinates())
+			.videoUrl(videoMetadataUploadRequest.getVideoUrl())
+			.thumbnailUrl(videoMetadataUploadRequest.getThumbnailUrl())
+			.title(videoMetadataUploadRequest.getTitle())
+			.description(videoMetadataUploadRequest.getDescription())
+			.build();
+		int result = videoMapper.insertVideo(videoInsertModel);
+		if (result != 1) {
+			throw new RuntimeException("Failed to insert video metadata");
+		}
+	}
 
-    public VideoMetadataResponse getVideoMetadata(Long videoId) {
-        VideoModel videoModel = videoMapper.selectVideoByVideoId(videoId);
-        VideoMetadata videoMetadata = convertVideoModelToVideoMetadata(videoModel);
-        return new VideoMetadataResponse(videoMetadata);
-    }
+	public VideoMetadataResponse getVideoMetadata(Long videoId) {
+		VideoModel videoModel = videoMapper.selectVideoByVideoId(videoId);
+		VideoMetadata videoMetadata = convertVideoModelToVideoMetadata(videoModel);
+		return new VideoMetadataResponse(videoMetadata);
+	}
 
-    public VideoMetadataListResponse getVideoMetadataListOfTravel(Long travelId) {
-        List<VideoModel> videoModelList = videoMapper.selectVideosByTravelId(travelId);
-        List<VideoMetadata> videoMetadataList = videoModelList.stream().map(this::convertVideoModelToVideoMetadata).toList();
-        return new VideoMetadataListResponse(videoMetadataList);
-    }
+	public VideoMetadataListResponse getVideoMetadataListOfTravel(Long travelId) {
+		List<VideoModel> videoModelList = videoMapper.selectVideosByTravelId(travelId);
+		List<VideoMetadata> videoMetadataList = videoModelList.stream()
+			.map(this::convertVideoModelToVideoMetadata)
+			.toList();
+		return new VideoMetadataListResponse(videoMetadataList);
+	}
 
-    public VideoMetadataListResponse getVideoMetadataListOfMember(String publicId) {
-        List<VideoModel> videoModelList = videoMapper.selectVideosByMemberId(memberUtil.getMemberIdByPublicId(publicId));
-        List<VideoMetadata> videoMetadataList = videoModelList.stream().map(this::convertVideoModelToVideoMetadata).toList();
-        return new VideoMetadataListResponse(videoMetadataList);
-    }
+	public VideoMetadataListResponse getVideoMetadataListOfMember(String publicId) {
+		List<VideoModel> videoModelList = videoMapper.selectVideosByMemberId(
+			memberUtil.getMemberIdByPublicId(publicId));
+		List<VideoMetadata> videoMetadataList = videoModelList.stream()
+			.map(this::convertVideoModelToVideoMetadata)
+			.toList();
+		return new VideoMetadataListResponse(videoMetadataList);
+	}
 
-    private VideoMetadata convertVideoModelToVideoMetadata(VideoModel videoModel) {
-        return VideoMetadata.builder()
-                .videoId(videoModel.getVideoId())
-                .publicId(memberUtil.getPublicIdByMemberId(videoModel.getMemberId()))
-                .travelId(videoModel.getTravelId())
-                .coordinates(videoModel.getCoordinates())
-                .videoUrl(videoModel.getVideoUrl())
-                .thumbnailUrl(videoModel.getThumbnailUrl())
-                .title(videoModel.getTitle())
-                .description(videoModel.getDescription())
-                .createdAt(videoModel.getCreatedAt())
-                .build();
-    }
+	public VideoMetadataListResponse getVideoMetadataListByLocationAndDistance(String coordinates, Integer distance) {
+		if (distance == null) {
+			distance = 10;
+		}
+		List<VideoModel> videoModelList = videoMapper.searchVideosByCoordinatesWithinDistance(coordinates, distance);
+		List<VideoMetadata> videoMetadataList = videoModelList.stream()
+			.map(this::convertVideoModelToVideoMetadata)
+			.toList();
+		return new VideoMetadataListResponse(videoMetadataList);
+	}
+
+	private VideoMetadata convertVideoModelToVideoMetadata(VideoModel videoModel) {
+		return VideoMetadata.builder()
+			.videoId(videoModel.getVideoId())
+			.publicId(memberUtil.getPublicIdByMemberId(videoModel.getMemberId()))
+			.travelId(videoModel.getTravelId())
+			.coordinates(videoModel.getCoordinates())
+			.videoUrl(videoModel.getVideoUrl())
+			.thumbnailUrl(videoModel.getThumbnailUrl())
+			.title(videoModel.getTitle())
+			.description(videoModel.getDescription())
+			.createdAt(videoModel.getCreatedAt())
+			.build();
+	}
 }
