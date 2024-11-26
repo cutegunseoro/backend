fdb9ad135d05f5199c6dfa9c0883ace8ebb728f3
Gwon Hong <gwonhong.dev@gmail.com>
2024-11-27 05:32:18 +0900

    update CORS

--- src/main/java/com/ssafy/travlog/api/config/SecurityConfiguration.java
+++ src/main/java/com/ssafy/travlog/api/config/SecurityConfiguration.java
@@ -1,6 +1,9 @@
 package com.ssafy.travlog.api.config;
 
-import io.jsonwebtoken.security.Keys;
+import java.util.Arrays;
+
+import javax.crypto.SecretKey;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,76 +21,75 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import javax.crypto.SecretKey;
-import java.util.Arrays;
+import io.jsonwebtoken.security.Keys;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-    private final SecretKey key;
+	private final SecretKey key;
 
-    public SecurityConfiguration(@Value("${jwt.secret}") String secret) {
-        this.key = Keys.hmacShaKeyFor(secret.getBytes());
-    }
+	public SecurityConfiguration(@Value("${jwt.secret}") String secret) {
+		this.key = Keys.hmacShaKeyFor(secret.getBytes());
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-//                .cors((cors) -> {
-//                    cors.configurationSource(new CorsConfigurationSource() {
-//                        @Override
-//                        public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-//                            CorsConfiguration corsConfiguration = new CorsConfiguration();
-//                            corsConfiguration.addAllowedOriginPattern("http://localhost:5173");
-//                            corsConfiguration.addAllowedMethod("*");
-//                            corsConfiguration.addAllowedHeader("*");
-//                            corsConfiguration.setAllowCredentials(true);
-//                            return corsConfiguration;
-//                        }
-//                    });
-//                })
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf((csrf) -> {
-                    csrf.disable();
-                })
-                .authorizeHttpRequests((authorize) -> {
-                    authorize.requestMatchers("/auth/*").permitAll();
-//                    authorize.requestMatchers("/members/{publicId}").permitAll();
-                    authorize.anyRequest().authenticated();
-                })
-                .sessionManagement((session) -> {
-                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-                })
-                .oauth2ResourceServer((oauth2) -> {
-                    oauth2.jwt(Customizer.withDefaults());
-                });
-        return http.build();
-    }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			//                .cors((cors) -> {
+			//                    cors.configurationSource(new CorsConfigurationSource() {
+			//                        @Override
+			//                        public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+			//                            CorsConfiguration corsConfiguration = new CorsConfiguration();
+			//                            corsConfiguration.addAllowedOriginPattern("http://localhost:5173");
+			//                            corsConfiguration.addAllowedMethod("*");
+			//                            corsConfiguration.addAllowedHeader("*");
+			//                            corsConfiguration.setAllowCredentials(true);
+			//                            return corsConfiguration;
+			//                        }
+			//                    });
+			//                })
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+			.csrf((csrf) -> {
+				csrf.disable();
+			})
+			.authorizeHttpRequests((authorize) -> {
+				authorize.requestMatchers("/auth/*").permitAll();
+				//                    authorize.requestMatchers("/members/{publicId}").permitAll();
+				authorize.anyRequest().authenticated();
+			})
+			.sessionManagement((session) -> {
+				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+			})
+			.oauth2ResourceServer((oauth2) -> {
+				oauth2.jwt(Customizer.withDefaults());
+			});
+		return http.build();
+	}
 
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withSecretKey(this.key).build();
-    }
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		return NimbusJwtDecoder.withSecretKey(this.key).build();
+	}
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return webSecurity -> webSecurity.ignoring().requestMatchers("/auth/*");
-    }
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return webSecurity -> webSecurity.ignoring().requestMatchers("/auth/*");
+	}
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드
-        configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더 허용
-        configuration.setAllowCredentials(true); // 쿠키 허용
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 설정 적용
-        return source;
-    }
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5173", "http://localhost:4173"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드
+		configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더 허용
+		configuration.setAllowCredentials(true); // 쿠키 허용
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 설정 적용
+		return source;
+	}
 }
--- src/main/java/com/ssafy/travlog/api/config/WebConfiguration.java
+++ src/main/java/com/ssafy/travlog/api/config/WebConfiguration.java
@@ -6,12 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:5173", "http://localhost:4173")
+			.allowedMethods("*")
+			.allowedHeaders("*")
+			.allowCredentials(true);
+	}
 }
